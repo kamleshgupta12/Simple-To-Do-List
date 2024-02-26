18d592f1e2cd9889f3281c5f25a54cddb3fd2f3a
kamleshgupta12 <bittugupta2743@gmail.com>
2024-02-26 09:19:18 +0530

    to-do list

---
+++ index.js
@@ -0,0 +1,65 @@
+var inputTag = document.getElementById("input");
+var listItems = document.getElementById("list");
+// console.log(listItems);
+
+function add() {
+    if (inputTag.value === '') {
+        alert("Please Enter Task...")
+    }
+    else {
+        const taskInput = document.getElementById('input');
+        const taskList = document.getElementById('list');
+        // console.log(taskList);
+
+        // create li items 
+
+        const createLi = document.createElement('li');
+        createLi.innerHTML = taskInput.value;
+        // console.log(createLi);
+
+
+
+        //  create delete btn 
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+
+
+        // console.log(deleteButton);
+
+        deleteButton.addEventListener('click', function () {
+            taskList.removeChild(createLi);
+        });
+
+        createLi.appendChild(deleteButton);
+
+
+        //   create update btn 
+
+        const updateButton = document.createElement('button');
+        updateButton.textContent = 'Update';
+        // console.log(updateButton);
+         updateButton.style.backgroundColor = "green";
+
+        updateButton.addEventListener('click', function () {
+            const updateTask = prompt('Please Enter New Task....');
+            if (updateTask !== '') {
+                createLi.innerHTML = updateTask;
+                createLi.appendChild(deleteButton);
+                createLi.appendChild(updateButton);
+              
+            }
+        });
+
+
+        createLi.appendChild(deleteButton);
+        createLi.appendChild(updateButton);
+        taskList.appendChild(createLi);
+        taskInput.value = '';
+
+    }
+}
+
+
+
+
